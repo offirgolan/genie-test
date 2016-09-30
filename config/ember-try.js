b55350683a488c272eb7335c7aba5e3237da2843
Offir Golan <offirgolan@gmail.com>
2016-09-30 17:39:40 +1300

    ember-try config management

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,10 +1,21 @@
 /*jshint node:true*/
 
 module.exports = {
+  useVersionCompatibility: true,
   scenarios: [{
     name: 'default',
     bower: {
       dependencies: {}
+    }
+  }, {
+    name: 'ember-lts-2.4',
+    bower: {
+      dependencies: {
+        'ember': 'components/ember#lts-2-4'
+      },
+      resolutions: {
+        'ember': 'lts-2-4'
+      }
     }
   }, {
     name: 'ember-release',
@@ -19,24 +30,8 @@ module.exports = {
   }, {
     name: 'ember-beta',
     allowedToFail: true,
-    bower: {
-      dependencies: {
-        'ember': 'components/ember#beta'
-      },
-      resolutions: {
-        'ember': 'beta'
-      }
-    }
   }, {
     name: 'ember-canary',
     allowedToFail: true,
-    bower: {
-      dependencies: {
-        'ember': 'components/ember#canary'
-      },
-      resolutions: {
-        'ember': 'canary'
-      }
-    }
   }]
 };
---
+++ config/scenarios/unstable.js
@@ -0,0 +1,27 @@
+/*jshint node:true*/
+
+module.exports = {
+  scenarios: [{
+    name: 'ember-beta',
+    allowedToFail: true,
+    bower: {
+      dependencies: {
+        'ember': 'components/ember#beta'
+      },
+      resolutions: {
+        'ember': 'beta'
+      }
+    }
+  }, {
+    name: 'ember-canary',
+    allowedToFail: true,
+    bower: {
+      dependencies: {
+        'ember': 'components/ember#canary'
+      },
+      resolutions: {
+        'ember': 'canary'
+      }
+    }
+  }]
+};
