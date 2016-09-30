ea173d47d65bd992b944655f7cbf93b8a72198d5
Offir Golan <offirgolan@gmail.com>
2016-09-30 17:53:39 +1300

    simpler try config

--- config/ember-try.js
+++ config/ember-try.js
@@ -30,8 +30,24 @@ module.exports = {
   }, {
     name: 'ember-beta',
     allowedToFail: true,
+    bower: {
+      dependencies: {
+        'ember': 'components/ember#beta'
+      },
+      resolutions: {
+        'ember': 'beta'
+      }
+    }
   }, {
     name: 'ember-canary',
     allowedToFail: true,
+    bower: {
+      dependencies: {
+        'ember': 'components/ember#canary'
+      },
+      resolutions: {
+        'ember': 'canary'
+      }
+    }
   }]
 };
